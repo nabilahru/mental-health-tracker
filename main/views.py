07e560066c830969497b1eb54dc539453cfe99ea
nabilahru <nabilahroslitau@gmail.com>
2024-11-13 14:45:21 +0700

    Tutorial 8: Flutter Networking, Authentication, and Integration

--- main/views.py
+++ main/views.py
@@ -13,6 +13,8 @@ from django.contrib.auth.decorators import login_required
 from django.views.decorators.csrf import csrf_exempt
 from django.views.decorators.http import require_POST
 from django.utils.html import strip_tags
+import json
+from django.http import JsonResponse
 
 @login_required(login_url='/login')
 def show_main(request):
@@ -70,6 +72,24 @@ def edit_mood(request, id):
 
     context = {'form': form}
     return render(request, "edit_mood.html", context)
+
+@csrf_exempt
+def create_mood_flutter(request):
+    if request.method == 'POST':
+
+        data = json.loads(request.body)
+        new_mood = MoodEntry.objects.create(
+            user=request.user,
+            mood=data["mood"],
+            mood_intensity=int(data["mood_intensity"]),
+            feelings=data["feelings"]
+        )
+
+        new_mood.save()
+
+        return JsonResponse({"status": "success"}, status=200)
+    else:
+        return JsonResponse({"status": "error"}, status=401)
 
 def delete_mood(request, id):
     # Get mood berdasarkan id
